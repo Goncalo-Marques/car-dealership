4641f920cb4002de9f4d388c8bbe711bc859f5a5
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2021-12-23 15:04:37 +0000

    [upd] database settings

--- car-dealership-backend/car_dealership_backend/settings.py
+++ car-dealership-backend/car_dealership_backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'car_dealership_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'car_dealership',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
