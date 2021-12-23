09f32f012511ae25df2ab8b4fb7e7ec1cafdfbce
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2021-12-23 16:34:00 +0000

    [new] initial models

--- car-dealership-backend/car_dealership/models.py
+++ car-dealership-backend/car_dealership/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=20, null=False)
+
+    class Meta:
+        managed = True
+        db_table = "brand"
+
+
+class Client(models.Model):
+    full_name = models.CharField(max_length=50, null=False)
+    birthdate = models.DateField(null=False)
+    phone_number = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=50, null=False)
+
+    class Meta:
+        managed = True
+        db_table = "client"
+
+
+class Car(models.Model):
+    id_brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, db_column="id_brand", null=False
+    )
+    id_client = models.ForeignKey(
+        Client,
+        on_delete=models.SET_DEFAULT,
+        db_column="id_client",
+        default=None,
+        blank=True,
+        null=True,
+    )
+    name = models.CharField(max_length=50, null=False)
+    new = models.BooleanField(default=True, null=False)
+    doors = models.IntegerField(default=3, null=False)
+    cubic_capacity = models.IntegerField(null=False)
+    power = models.IntegerField(null=False)
+
+    class Meta:
+        managed = True
+        db_table = "car"
