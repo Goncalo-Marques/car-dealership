0a589a8a4df604195d7ca5f6c7fd4f7423b0d17c
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2021-12-24 17:37:51 +0000

    [upd] code format

--- car-dealership-backend/car_dealership/serializers/brand.py
+++ car-dealership-backend/car_dealership/serializers/brand.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from car_dealership.models import Brand
 
+
 class BrandSerializer(serializers.ModelSerializer):
-     class Meta:
+    class Meta:
         model = Brand
         fields = "__all__"
