9d86d8466b0c84a6f4ee0d0ff3a2e7d086e2da2e
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2021-12-24 18:14:38 +0000

    [upd] code format

--- car-dealership-backend/car_dealership/urls.py
+++ car-dealership-backend/car_dealership/urls.py
@@ -3,11 +3,12 @@ from car_dealership.views.client import *
 from car_dealership.views.brand import *
 
 urlpatterns = [
-    path('client/',  Client.as_view()),
-    path('client/<int:pk>/', ClientByID.as_view()),
-    path('clients/', Clients.as_view()),
-
-    path('brand/', Brand.as_view()),
-    path('brand/<pk>', BrandByName.as_view()),
-    path('brands/', Brands.as_view()),
+    # client
+    path("client/", Client.as_view()),
+    path("client/<int:pk>/", ClientByID.as_view()),
+    path("clients/", Clients.as_view()),
+    # brand
+    path("brand/", Brand.as_view()),
+    path("brand/<str:pk>", BrandByName.as_view()),
+    path("brands/", Brands.as_view()),
 ]
