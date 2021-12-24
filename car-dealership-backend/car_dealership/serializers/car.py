fd53bff78be515c5d3afb253143ed70f198571e3
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2021-12-24 17:36:51 +0000

    [new] car serializer

--- car-dealership-backend/car_dealership/serializers/car.py
+++ car-dealership-backend/car_dealership/serializers/car.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
+from car_dealership.models import Car
 
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = "__all__"
