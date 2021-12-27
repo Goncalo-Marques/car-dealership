6c528610353bda100d287a2a3e3503f7f0c36c54
Ricardo Vieira <vieiraricardodias@gmail.com>
2021-12-27 23:04:01 +0000

    [new] simple view

--- car-dealership-frontend/car_dealership/views.py
+++ car-dealership-frontend/car_dealership/views.py
@@ -1,3 +1,9 @@
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def clients(request):
+   # get the list of clients
+   response = requests.get('http://127.0.0.1:8000/clients/')
+   # transform the response to json objects
+   clients = response.json()
+   return render(request, "clients.html", {"clients": clients})
