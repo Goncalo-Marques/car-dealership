0d2dc45a2094e85b43c22da91103ddcfedded299
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-19 19:49:17 +0000

    [new] initial admin pages

--- car-dealership-backend/car_dealership/admin.py
+++ car-dealership-backend/car_dealership/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Brand)
+class BrandAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "birthdate")
+
+
+@admin.register(models.Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("name", "brand", "id_client")
