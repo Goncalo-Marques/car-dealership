bcba833cab8375259d5f8043881cd6a5f5ff196c
Ricardo Vieira <vieiraricardodias@gmail.com>
2022-01-19 21:32:50 +0000

    [new] car page

--- car-dealership-frontend/car_dealership/views.py
+++ car-dealership-frontend/car_dealership/views.py
@@ -7,3 +7,10 @@ def clients(request):
    # transform the response to json objects
    clients = response.json()
    return render(request, "clients.html", {"clients": clients})
+
+def cars(request):
+   # get the list of cars
+   response = requests.get('http://127.0.0.1:8000/cars/')
+   # transform the response to json objects
+   cars = response.json()
+   return render(request, "cars.html", {"cars": cars})
