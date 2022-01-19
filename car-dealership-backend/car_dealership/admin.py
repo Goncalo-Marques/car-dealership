e044088fbe999d315a1931e25e059dfa4f88015d
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-19 20:56:48 +0000

    [upd] final admin page

--- car-dealership-backend/car_dealership/admin.py
+++ car-dealership-backend/car_dealership/admin.py
@@ -1,17 +1,48 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.utils.html import format_html
 from . import models
 
 
 @admin.register(models.Brand)
 class BrandAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name",)
+    search_fields = ("name",)
 
 
 @admin.register(models.Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ("full_name", "birthdate")
+    list_display = ("full_name", "view_cars_link")
+    search_fields = ("full_name",)
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def view_cars_link(self, obj):
+        url = (
+            reverse("admin:car_dealership_car_changelist")
+            + "?"
+            + urlencode({"id_client": f"{obj.id}"})
+        )
+        count = obj.car_set.count()
+        return format_html('<a href="{}">{} Car(s)</a>', url, count)
+
+    view_cars_link.short_description = "cars"
 
 
 @admin.register(models.Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ("name", "brand", "id_client")
+    list_display = ("name", "brand", "view_client_link")
+    search_fields = ("name",)
+    list_filter = ("id_client",)
+
+    def view_client_link(self, obj):
+        if obj.id_client == None:
+            return None
+
+        url = reverse("admin:car_dealership_client_change", args=[obj.id_client.id])
+        clientName = obj.id_client
+        return format_html('<a href="{}">{}</a>', url, clientName)
+
+    view_client_link.short_description = "client"
