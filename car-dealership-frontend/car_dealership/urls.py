3b2080fb7f7e3bf3c93a9dad6a09294e00940b2f
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-22 16:12:07 +0000

    [fix] bootstrap version

--- car-dealership-frontend/car_dealership/urls.py
+++ car-dealership-frontend/car_dealership/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.clients, name="index"),
+    path("", views.index, name="index"),
     path("clients/", views.clients, name="clients"),
     path("cars/", views.cars, name="cars"),
 ]
