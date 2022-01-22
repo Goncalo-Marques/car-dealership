48ac8236471e0da29aa34cccbfea34aaa21009e5
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-22 17:29:22 +0000

    [wip] base html

--- car-dealership-frontend/car_dealership/urls.py
+++ car-dealership-frontend/car_dealership/urls.py
@@ -3,10 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # index
     path("", views.index, name="index"),
-
-    path("account/", views.account, name="account/"),
-
+    # client
+    path("account/", views.account, name="account"),
+    path("logIn/", views.account, name="logIn"),
+    path("signUp/", views.account, name="signUp"),
+    path("logOut/", views.account, name="logOut"),
     path("clients/", views.clients, name="clients"),
-    path("cars/", views.cars, name="cars"),
+    # car
+    path("newCars/", views.cars, name="newCars"),
+    path("usedCars/", views.cars, name="usedCars"),
+    path("myCars/", views.cars, name="myCars"),
 ]
--- car-dealership-frontend/car_dealership/views.py
+++ car-dealership-frontend/car_dealership/views.py
@@ -2,22 +2,57 @@ import requests
 from urllib.parse import urljoin
 from django.shortcuts import render
 
+# API base url
 BASE_URL = "http://127.0.0.1:8000/"
 
+# maximum number of featured cars
+MAX_FEATURED_CARS = 5
+
+
 def index(request):
-    return render(request, "index.html", None)
+    # get the list of cars not sold
+    response = requests.get(urljoin(BASE_URL, "carsNotSold/"))
+
+    # transform the response to json object
+    cars = response.json()
+
+    # featured cars
+    featuredCars = []
+    featuredCount = 0
+
+    # other cars
+    otherCars = []
+
+    for car in cars:
+        # checks if the car belongs to the featured category
+        if car["new"] == True and featuredCount < 5:
+            featuredCars.append(car)
+            featuredCount += 1
+        else:
+            otherCars.append(car)
+
+    context = {
+        "client": None,
+        "featured": featuredCars,
+        "cars": otherCars,
+    }
+    return render(request, "index.html", context)
+
 
 def account(request):
     # get client
     response = requests.get(urljoin(BASE_URL, "client/1/"))
+
     # transform the response to json object
     client = response.json()
     return render(request, "account.html", {"client": client})
 
+
 def clients(request):
     # get the list of clients
     response = requests.get(urljoin(BASE_URL, "clients/"))
-    # transform the response to json objects
+
+    # transform the response to json object
     clients = response.json()
     return render(request, "clients.html", {"clients": clients})
 
@@ -25,7 +60,8 @@ def clients(request):
 def cars(request):
     # get the list of cars
     response = requests.get(urljoin(BASE_URL, "cars/"))
-    # transform the response to json objects
+
+    # transform the response to json object
     cars = response.json()
     for car in cars:
         print(car["new"])
