3c495dd6053d84d35b26e644191f8ac4df7db98c
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-23 14:56:59 +0000

    [upd] validate client phone number

--- car-dealership-backend/car_dealership/views/client.py
+++ car-dealership-backend/car_dealership/views/client.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from car_dealership import models
 from car_dealership.serializers.client import ClientSerializer
 from django.http import Http404
@@ -12,14 +13,26 @@ def get_object(pk):
     except models.Client.DoesNotExist:
         raise Http404
 
+
+# checks if the client has any invalid fields
+def is_valid(object):
+    if object["phone_number"] < 900000000 or object["phone_number"] > 999999999:
+        return False
+    return True
+
+
 # view to create a new client
 class Client(APIView):
     def post(self, request, format=None):
         serializer = ClientSerializer(data=request.data)
         if serializer.is_valid():
+            if not is_valid(serializer.validated_data):
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # view to fetch, update and delete a client by ID
 class ClientByID(APIView):
@@ -32,6 +45,9 @@ class ClientByID(APIView):
         client = get_object(pk)
         serializer = ClientSerializer(client, data=request.data)
         if serializer.is_valid():
+            if not is_valid(serializer.validated_data):
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -40,6 +56,7 @@ class ClientByID(APIView):
         client = get_object(pk)
         client.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # view to fetch all existing clients
 class Clients(APIView):
