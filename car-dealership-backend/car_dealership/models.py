4e73766c0582919695fe0de8877875fd2582073f
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-26 21:29:09 +0000

    [cut] unwanted line of code

--- car-dealership-backend/car_dealership/models.py
+++ car-dealership-backend/car_dealership/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-
 from .managers import CustomUserManager
 
 # brand model
