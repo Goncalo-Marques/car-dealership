a8747b20898ea4408347633737de33cba429afb3
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-29 17:26:37 +0000

    [fix] email validation

--- car-dealership-backend/car_dealership/serializers/client.py
+++ car-dealership-backend/car_dealership/serializers/client.py
@@ -13,9 +13,20 @@ class ClientSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def validate(self, data):
+        id = self.instance.id if self.instance != None else None
+
+        # validating email
+        email = data.get("email", None)
+        if email != None:
+            record = Client.objects.filter(email=email).first()
+            if record != None:
+                if id == None or (id != None and record.id != id):
+                    raise serializers.ValidationError("Email already exists")
+
         # validating phone number
-        if "phone_number" in data and (
-            data["phone_number"] < 900000000 or data["phone_number"] > 999999999
+        phone_number = data.get("phone_number", None)
+        if phone_number != None and (
+            phone_number < 900000000 or phone_number > 999999999
         ):
             raise serializers.ValidationError("Invalid phone number")
 
@@ -29,18 +40,11 @@ class ClientSerializer(serializers.ModelSerializer):
         return super(ClientSerializer, self).create(validated_data)
 
     def update(self, instance, validated_data):
+        instance.email = validated_data.get("email", instance.email)
         instance.full_name = validated_data.get("full_name", instance.full_name)
         instance.birthdate = validated_data.get("birthdate", instance.birthdate)
         instance.address = validated_data.get("address", instance.address)
-        instance.phone_number = validated_data.get(
-            "phone_number", instance.phone_number
-        )
-
-        # checking if email is not already being used
-        email = validated_data.get("email", instance.email)
-        record = Client.objects.filter(email=email).first()
-        if not record:
-            instance.email = email
+        instance.phone_number = validated_data.get("phone_number", None)
 
         # encrypting the password
         password = validated_data.get("password", None)
