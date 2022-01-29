c4cb62256a764f03db497980a896d24154a59cf1
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-29 19:04:27 +0000

    [upd] generic database configuration

--- car-dealership-backend/car_dealership_backend/settings.py
+++ car-dealership-backend/car_dealership_backend/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "car_dealership",
-        "USER": "marques",
-        "PASSWORD": "admin",
+        "USER": "user",
+        "PASSWORD": "password",
         "HOST": "localhost",
         "PORT": "5432",
     }
